5a53dad91957dc01952f4ef2a3a7886cdc41c90f
Ibrahim Achkar <ibaemail@gmail.com>
2021-12-16 20:59:17 +1100

    basic seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "destroying all users and plans, bye bob!"
+
+User.destroy_all
+Plan.destroy_all
+
+puts "creating new users and plan"
+
+bob = User.create(username: 'Bob', email: 'bob@mail.com', password: 'aaaaaa')
+bob.save!
+
+puts "User #{bob.username} created with id #{bob.id}!"
+
+gary = User.create(username: 'Gazza', email: 'gazza@mail.com', password: 'aaaaaa')
+gary.save!
+
+puts "User #{gary.username} created with id #{gary.id}!"
+
+coding = Plan.create(name: "Learning JS", user_id: bob.id)
+coding.save!
+
+puts "Plan #{coding.name} created with id #{coding.id}!"
+
+running = Plan.create(name: "Learning to Run!", user_id: gary.id)
+running.save!
+
+puts "Plan #{coding.name} created with id #{coding.id}!"
+
+puts "done!"
