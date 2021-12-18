f06e6823a7cbe1a5339b282e3ffe9db2bd39b479
Andrew Kurjan <andrewkurjan@gmail.com>
2021-12-18 14:37:06 +1100

    added search and display all functionality

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -2,6 +2,11 @@ class PlansController < ApplicationController
 
   def index
     @plans = Plan.all
+    if params[:search]
+      @plans = Plan.search(params[:search]).order("created_at DESC")
+    else
+      @plans = Plan.all.order('created_at DESC')
+    end
   end
 
   def show
