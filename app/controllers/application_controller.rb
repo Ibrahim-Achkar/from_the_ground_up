799ebdfd3ee78b5190a931a2cbd4b8dfd0e9a05a
Ibrahim Achkar <ibaemail@gmail.com>
2021-12-18 14:51:00 +1100

    removed email from attributes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    attributes = %i[username email photo]
+    attributes = %i[username photo]
     devise_parameter_sanitizer.permit(:sign_up, keys: [attributes])
     devise_parameter_sanitizer.permit(:account_update, keys: [attributes])
   end
