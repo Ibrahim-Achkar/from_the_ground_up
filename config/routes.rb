8c101552c51644503d1385a98bef0fc243602f38
Ellie Stevens <elliebstevens@gmail.com>
2022-01-04 19:56:36 +1100

    added MVC for Resources

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
 
   resources :plans do
-    resources :resources, only: %i[create destroy]
+    resources :resources, only: %i[new create destroy]
     resources :tasks, only: %i[create delete edit update destroy]
     resources :diary_entries
     resources :goals
