448618aba3068ea4e68874a394bd55bbd35fa749
Ibrahim Achkar <ibaemail@gmail.com>
2022-01-07 13:04:38 +1100

    minor changes

--- app/javascript/channels/index.js
+++ app/javascript/channels/index.js
@@ -1,11 +1,5 @@
 // Load all the channels within this directory and all subdirectories.
 // Channel files must be named *_channel.js.
 
-const channels = require.context('.', true, /_channel\.js$/)
-channels.keys().forEach(channels)
-
-
-const heart = document.getElementById("hrt");
-heart.addEventListener("click", (event) => {
-  event.currentTarget.classList.toggle("red_heart");
-});
+const channels = require.context('.', true, /_channel\.js$/);
+channels.keys().forEach(channels);
