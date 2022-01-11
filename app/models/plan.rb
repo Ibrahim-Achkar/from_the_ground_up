a630b3cdc172486b4ad16c083d982606d73d602e
Ibrahim Achkar <ibaemail@gmail.com>
2022-01-11 18:51:38 +1100

    removed likes from user and plan models

--- app/models/plan.rb
+++ app/models/plan.rb
@@ -9,8 +9,6 @@ class Plan < ApplicationRecord
   has_many :diary_entries, dependent: :destroy
   has_many :tasks, dependent: :destroy
   has_many :goals, dependent: :destroy
-  has_many :likes, dependent: :destroy
-  # has_many :liking_users, class_name: 'Users', through: :liked_plans
 
   def self.search(search)
     where("lower(name) LIKE ?", "%#{search}%")
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :plans, dependent: :destroy
-  has_many :likes, dependent: :destroy
   has_one_attached :photo
-  # has_many :plans, through: :liked_plans, as: :liked_plans
-  # has_many :liked_plans, class_name: 'Plans', through: :liked_plans
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
