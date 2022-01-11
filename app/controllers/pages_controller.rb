f00963045ea07da2799474f1c182c56e0ae93a52
Andrew Kurjan <andrewkurjan@gmail.com>
2022-01-11 21:13:46 +1100

    removed has many likes from plan model - no longer needed

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,13 +3,14 @@ class PagesController < ApplicationController
 
   def home
   end
+
   def dashboard
     if current_user
        @user = current_user
        @plans = @user.plans
     #   @likes = Like.where(user: @user)
-     else
+    else
        redirect_to new_user_session_url
-     end
+    end
   end
 end
