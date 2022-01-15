805a5f47f52cb8bdf9f44bb700a832e1655298c3
vishuddha vithanage <dvishuddha@gmail.com>
2022-01-15 14:27:56 +1100

    fix the dash  oard stlye and link

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,6 @@
 class PagesController < ApplicationController
   #skip_before_action :authenticate_user!, only: [ :home ]
-
+  helper_method :find_icon
   def home
   end
 
@@ -12,5 +12,24 @@ class PagesController < ApplicationController
     else
        redirect_to new_user_session_url
     end
+  end
+
+  def find_icon(plan)
+      icons = {
+      "Art" => "fas fa-palette",
+      "Cooking" => "fas fa-utensils",
+      "Finance" => "fas fa-money-bill-alt",
+      "Gaming" => "fas fa-gamepad",
+      "Gardening" => "fas fa-leaf",
+      "Health & Wellbeing" => "fas fa-notes-medical",
+      "Language" => "fas fa-language",
+      "Misc" => "fas fa-random",
+      "Music" => "fas fa-music",
+      "Professional Development" => "fas fa-medal",
+      "Science" => "fas fa-microscope",
+      "Sport & Fitness" => "far fa-futbol",
+      "Tech" => "fas fa-laptop"
+    }
+    icons[plan]
   end
 end
