bb413b81dd3957491ed28e40fa3bd502fdf8ae85
Ibrahim Achkar <ibaemail@gmail.com>
2022-01-18 20:40:35 +1100

    first version of meta tags, will need to be discussed with team and tweaked

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,12 @@ class ApplicationController < ActionController::Base
 
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  def after_sign_in_path_for(resource)
+  def after_sign_in_path_for(_resource)
     dashboard_path
+  end
+
+  def default_url_options
+    { host: ENV["DOMAIN"] || "localhost:3000" }
   end
 
   protected
@@ -14,5 +18,4 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [attributes])
     devise_parameter_sanitizer.permit(:account_update, keys: [attributes])
   end
-
 end
