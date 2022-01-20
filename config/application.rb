e3ca184b28154bc050fd433ff816f4810e968acd
Andrew Kurjan <andrewkurjan@gmail.com>
2022-01-20 20:29:57 +1100

    added 404 and 500 error pages

--- config/application.rb
+++ config/application.rb
@@ -15,7 +15,7 @@ module FromTheGroundUp
     end
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
-
+    config.exceptions_app = self.routes
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'errors/not_found'
+  get 'errors/internal_server_error'
   devise_for :users
   root to: 'pages#home'
   get '/dashboard', to: 'pages#dashboard'
@@ -18,5 +20,8 @@ Rails.application.routes.draw do
     resources :diary_entries
     resources :goals, only: %i[new create edit update delete destroy]
   end
+
+  match "/404", to: "errors#not_found", as: 'not_found', via: :all
+  match "/500", to: "errors#internal_server_error", as: 'internal_server_error', via: :all
 
 end
